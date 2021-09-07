5bce96821e619376793e2b3ac1ba3d27b0797569
Nuris Akbar <nuris.akbar@gmail.com>
2021-09-07 17:02:45 +0700

    Update Alert.php

--- app/View/Components/Sbadmin/Alert.php
+++ app/View/Components/Sbadmin/Alert.php
@@ -6,41 +6,41 @@ use Illuminate\View\Component;
 
 class Alert extends Component
 {
-  /**
-   * The alert type.
-   *
-   * @var string
-   */
-  public $type;
+    /**
+     * The alert type.
+     *
+     * @var string
+     */
+    public $type;
 
-  /**
-   * The alert message.
-   *
-   * @var string
-   */
-  public $message;
+    /**
+     * The alert message.
+     *
+     * @var string
+     */
+    public $message;
 
-  /**
-   * Create the component instance.
-   *
-   * @param  string  $type
-   * @param  string  $message
-   * @return void
-   */
-  public function __construct($type = null, $message = null)
-  {
-    $this->type = $type;
-    $this->message = $message;
-  }
+    /**
+     * Create the component instance.
+     *
+     * @param  string  $type
+     * @param  string  $message
+     * @return void
+     */
+    public function __construct($type = null, $message = null)
+    {
+        $this->type = $type;
+        $this->message = $message;
+    }
 
 
-  /**
-   * Get the view / contents that represent the component.
-   *
-   * @return \Illuminate\Contracts\View\View|string
-   */
-  public function render()
-  {
-    return view('components.sbadmin.alert');
-  }
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.sbadmin.alert');
+    }
 }
