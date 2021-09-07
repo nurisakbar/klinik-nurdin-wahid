08590450d03a6534b650697ae79900267fe1b43c
Nuris Akbar <nuris.akbar@gmail.com>
2021-09-07 17:08:47 +0700

    int

--- app/Http/Controllers/Admin/Pasien/TambahPasienController.php
+++ app/Http/Controllers/Admin/Pasien/TambahPasienController.php
@@ -11,15 +11,15 @@ use Illuminate\Http\Request;
 
 class TambahPasienController extends Controller
 {
-  public function index()
-  {
-    $pendaftar = new Pendaftaran();
-    $polikliniks = Poliklinik::get();
-    return view("Pasien.tambah-pasien", compact("pendaftar", "polikliniks"));
-  }
-  public function store()
-  {
-    $attr =  request()->validate([
+    public function index()
+    {
+        $pendaftar = new Pendaftaran();
+        $polikliniks = Poliklinik::get();
+        return view("pasien.tambah-pasien", compact("pendaftar", "polikliniks"));
+    }
+    public function store()
+    {
+        $attr =  request()->validate([
       'nama' => "required|min:3",
       'email' => "nullable|min:3|email|unique:pasiens",
       "no_ktp" => "integer|required|min:16",
@@ -34,8 +34,8 @@ class TambahPasienController extends Controller
       "layanan" => "required",
     ]);
 
-    $attr['email']  = $attr['email'] ?? uniqid("pasien", true) . "@klinik.com";
-    $pasien = Pasien::create([
+        $attr['email']  = $attr['email'] ?? uniqid("pasien", true) . "@klinik.com";
+        $pasien = Pasien::create([
       "nama" => $attr['nama'],
       'email' => $attr['email'],
       'no_ktp' => $attr['no_ktp'],
@@ -45,8 +45,8 @@ class TambahPasienController extends Controller
       'tempat_lahir' => $attr['tempat_lahir'],
       'tanggal_lahir' => $attr['tanggal_lahir'],
     ]);
-    $nmrPendaftar = "PS-" . date("Ymd") . "-";
-    $pendaftaran =  $pasien->pendaftarans()->create([
+        $nmrPendaftar = "PS-" . date("Ymd") . "-";
+        $pendaftaran =  $pasien->pendaftarans()->create([
       'dokter_id' => $attr['dokter_id'],
       "nomor_pendaftaran" =>  getLastId(new Pendaftaran(), "nomor_pendaftaran", $nmrPendaftar),
       "layanan" => $attr['layanan'],
@@ -54,31 +54,31 @@ class TambahPasienController extends Controller
       "status_poli" => 0,
       "status_kasir" => 0,
     ]);
-    return redirect(route("pasien.detail", $pendaftaran->nomor_pendaftaran))->with("success", "Pasien Di tambahkan");
-  }
-  public function detail(Pendaftaran $pendaftaran)
-  {
-    return view("Pasien.details", compact("pendaftaran"));
-  }
-  public function terdaftar()
-  {
-    $pasiens =  PasienResaurce::collection(Pasien::all());
-    $polikliniks =  Poliklinik::latest()->get();
-    return view("Pasien.terdaftar", compact("pasiens", "polikliniks"));
-  }
-  public function terdaftarStore()
-  {
-    $attr = request()->validate([
+        return redirect(route("pasien.detail", $pendaftaran->nomor_pendaftaran))->with("success", "Pasien Di tambahkan");
+    }
+    public function detail(Pendaftaran $pendaftaran)
+    {
+        return view("Pasien.details", compact("pendaftaran"));
+    }
+    public function terdaftar()
+    {
+        $pasiens =  PasienResaurce::collection(Pasien::all());
+        $polikliniks =  Poliklinik::latest()->get();
+        return view("Pasien.terdaftar", compact("pasiens", "polikliniks"));
+    }
+    public function terdaftarStore()
+    {
+        $attr = request()->validate([
       "dokter_id" => "required",
       "pasien_id" => "required",
       "poliklinik_id" => "required",
       "layanan" => "required",
     ]);
-    $nmrPendaftar = "PS-" . date("Ymd") . "-";
-    $attr['nomor_pendaftaran'] = $nmrPendaftar;
-    $attr["status_poli"] = 0;
-    $attr["status_kasir"] = 0;
-    $pendaftaran = Pendaftaran::create($attr);
-    return redirect(route("pasien.detail", $pendaftaran->nomor_pendaftaran))->with("success", "Pasien Di tambahkan");
-  }
+        $nmrPendaftar = "PS-" . date("Ymd") . "-";
+        $attr['nomor_pendaftaran'] = $nmrPendaftar;
+        $attr["status_poli"] = 0;
+        $attr["status_kasir"] = 0;
+        $pendaftaran = Pendaftaran::create($attr);
+        return redirect(route("pasien.detail", $pendaftaran->nomor_pendaftaran))->with("success", "Pasien Di tambahkan");
+    }
 }
